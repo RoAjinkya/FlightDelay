7007692d1c2a7f9afcc5cbd64172990f13655c04
DEichEich <D.Eichman@outlook.com>
2017-03-29 13:49:41 -0400

    Updated Folder stucture

---
+++ scala/src/main/scala/HelloWorldSpark.scala
@@ -0,0 +1,15 @@
+import org.apache.spark.{SparkConf, SparkContext}
+
+/**
+  * Created by daniel on 3/23/17.
+  */
+object SparkHelloWorld {
+  def main(args: Array[String]){
+    val conf = new SparkConf().setAppName("Word Count").setMaster("local")
+    val sc = new SparkContext(conf)
+    val filesc = sc.textFile("test.txt")
+    val wc = filesc.flatMap(line => line.split(" ")).map(word => (word, 1)).reduceByKey(_+_)
+    wc.saveAsTextFile("wc_out.txt")
+  }
+
+}

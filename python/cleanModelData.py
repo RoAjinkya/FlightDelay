c3ee2167d9c9c97c562eace4fb837a35bd0e92d0
DEichEich <D.Eichman@outlook.com>
2017-04-19 10:29:41 -0400

    Created clean model data

--- python/cleanModelData.py
+++ python/cleanModelData.py
@@ -1,19 +1,14 @@
 import csv
 import os
-year = "2008"
-f_all = open("../data/raw/DOT_"+year+".csv","r")
-csv_all = csv.reader(f_all, delimiter=',')
-header = csv_all.__next__()
-month_col = 1
-#This loop is much slower as it loop the orginal data set 12 times
-#But it is pretty (and only run once)
-for m in range(1,13):
-    print (m)
-    f_month = open("../data/raw/DOT_"+year+"_"+str(m)+".csv","w",newline='')
-    csv_month = csv.writer(f_month, delimiter=',')
-    csv_month.writerow(header)
-    f_all.seek(1)#row 0 is header
-    for row in csv_all:
-        if(row[month_col]==str(m)):
-            csv_month.writerow(row)
+import pandas as pd
 
+year = "2008"
+subfolder = "test_data"
+
+df = pd.read_csv("../data/"+subfolder+"/DOT_2008_WH_test.csv")
+df["TotalDelay"]=df["ArrDelay"] + df["DepDelay"]#find total dealy
+df = df[["TotalDelay","DayOfWeek","Origin","Dest","OriginSnow","OriginPrcp","DestSnow","DestPrcp","Holiday"]]
+
+df.to_csv("../data/"+subfolder+"/DOT_2008_WHD_test.csv")
+print(df.head())
+ 

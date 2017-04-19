662f155cdd5299b1d1f2f88b7b8d22683cef39e4
DEichEich <D.Eichman@outlook.com>
2017-04-19 10:41:10 -0400

    cleanModelData.py changed ap code to int

--- python/cleanModelData.py
+++ python/cleanModelData.py
@@ -1,14 +1,18 @@
 import csv
 import os
 import pandas as pd
-
+def APCode2Num(code):
+    cd =  [str(ord(c)) for c in code]
+    return "".join(cd)#cd.join("")
 year = "2008"
 subfolder = "test_data"
 
 df = pd.read_csv("../data/"+subfolder+"/DOT_2008_WH_test.csv")
 df["TotalDelay"]=df["ArrDelay"] + df["DepDelay"]#find total dealy
 df = df[["TotalDelay","DayOfWeek","Origin","Dest","OriginSnow","OriginPrcp","DestSnow","DestPrcp","Holiday"]]
+df["Origin"] = df["Origin"].apply(lambda x:APCode2Num(x))
+df["Dest"] = df["Dest"].apply(lambda x:APCode2Num(x))
 
-df.to_csv("../data/"+subfolder+"/DOT_2008_WHD_test.csv")
 print(df.head())
  
+df.to_csv("../data/"+subfolder+"/DOT_2008_WHD_test.csv")
